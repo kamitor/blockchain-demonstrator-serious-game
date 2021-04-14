07428908d3dd88f324d8c6a2fc06f5cc9ae40a31
Unknown-GH <unknown.not.taken@gmail.com>
2021-04-14 10:22:01 +0200

    Test

---
+++ Blockchain Demonstrator/Blockchain Demonstrator Web App/Models/Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blockchain_Demonstrator_Web_App.Models
+{
+    public class Test
+    {
+    }
+}
