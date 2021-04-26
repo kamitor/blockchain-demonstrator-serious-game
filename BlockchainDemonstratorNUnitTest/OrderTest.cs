40c652a9f39517ebfa9a40eeb484895cf9953464
RijstPlukker <s1142800@student.windesheim.nl>
2021-04-26 16:09:30 +0200

    Fixed When_OrderVolumeLowerThan0_Expect0 test

--- BlockchainDemonstratorNUnitTest/OrderTest.cs
+++ BlockchainDemonstratorNUnitTest/OrderTest.cs
@@ -17,23 +17,24 @@ namespace BlockchainDemonstratorNUnitTest
 
         [TestCase(-1)]
         [TestCase(-20)]
+        [TestCase(-51)]
         [TestCase(-618)]
         public void When_OrderVolumeLowerThan0_Expect0(int volume)
         {
+            _order.Volume = volume;
 
-            Assert.Throws<ArgumentException>(() => { _order.Volume = volume; });
+            Assert.AreEqual(0, _order.Volume);
         }
 
         [TestCase(0)]
         [TestCase(1)]
         [TestCase(25)]
-        [TestCase(94)]
         [TestCase(678)]
         public void When_OrderVolumeGreaterOrEqualTo0_ExpectVolumeToBeValue(int volume)
         {
             _order.Volume = volume;
 
-            Assert.AreEqual(_order.Volume, volume);
+            Assert.AreEqual(volume, _order.Volume);
         }
     }
 }
