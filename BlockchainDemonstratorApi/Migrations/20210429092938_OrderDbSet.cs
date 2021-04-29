d420aface8100a1a26c1b8e2fc946ef06a369203
Unknown-GH <unknown.not.taken@gmail.com>
2021-04-29 11:32:49 +0200

    Migration for orders table

---
+++ BlockchainDemonstratorApi/Migrations/20210429092938_OrderDbSet.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlockchainDemonstratorApi.Migrations
+{
+    public partial class OrderDbSet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_Player_DeliveryToPlayerId",
+                table: "Order");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_Player_RequestForPlayerId",
+                table: "Order");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Player_Order_CurrentOrderId",
+                table: "Player");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Order",
+                table: "Order");
+
+            migrationBuilder.RenameTable(
+                name: "Order",
+                newName: "Orders");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Order_RequestForPlayerId",
+                table: "Orders",
+                newName: "IX_Orders_RequestForPlayerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Order_DeliveryToPlayerId",
+                table: "Orders",
+                newName: "IX_Orders_DeliveryToPlayerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Player_DeliveryToPlayerId",
+                table: "Orders",
+                column: "DeliveryToPlayerId",
+                principalTable: "Player",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Player_RequestForPlayerId",
+                table: "Orders",
+                column: "RequestForPlayerId",
+                principalTable: "Player",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Player_Orders_CurrentOrderId",
+                table: "Player",
+                column: "CurrentOrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Player_DeliveryToPlayerId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Player_RequestForPlayerId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Player_Orders_CurrentOrderId",
+                table: "Player");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders");
+
+            migrationBuilder.RenameTable(
+                name: "Orders",
+                newName: "Order");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_RequestForPlayerId",
+                table: "Order",
+                newName: "IX_Order_RequestForPlayerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_DeliveryToPlayerId",
+                table: "Order",
+                newName: "IX_Order_DeliveryToPlayerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Order",
+                table: "Order",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_Player_DeliveryToPlayerId",
+                table: "Order",
+                column: "DeliveryToPlayerId",
+                principalTable: "Player",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_Player_RequestForPlayerId",
+                table: "Order",
+                column: "RequestForPlayerId",
+                principalTable: "Player",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Player_Order_CurrentOrderId",
+                table: "Player",
+                column: "CurrentOrderId",
+                principalTable: "Order",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
