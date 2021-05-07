da35ce759a1d8beb92d7b1932850ad4c86b08cbb
yazan <s1122756@student.windesheim.nl>
2021-05-07 14:21:26 +0200

    first working version of factors

--- Blockchain Demonstrator Web App/Controllers/FactorsController.cs
+++ Blockchain Demonstrator Web App/Controllers/FactorsController.cs
@@ -38,60 +38,64 @@ namespace Blockchain_Demonstrator_Web_App.Controllers
             return BadRequest();
         }
 
-       /* // GET: Factors/Details/5
-        public async Task<IActionResult> Details(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var factors = await _context.Factors
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (factors == null)
-            {
-                return NotFound();
-            }
-
-            return View(factors);
-        }
-
-        // GET: Factors/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Factors/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,retailTransport,manuTransport,procTransport,farmerTransport,holdingFactor,roundIncrement,retailProductPrice,manuProductPrice,procProductPrice,farmerProductPrice,harvesterProductPrice,setupCost,initialCapital")] Factors factors)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(factors);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(factors);
-        }
+     //  // GET: Factors/Details/5
+     //   public async Task<IActionResult> Details(string id)
+     //   {
+     //       if (id == null)
+     //       {
+     //           return NotFound();
+     //       }
+     //
+     //       var factors = await _context.Factors
+     //           .FirstOrDefaultAsync(m => m.Id == id);
+     //       if (factors == null)
+     //       {
+     //           return NotFound();
+     //       }
+     //
+     //       return View(factors);
+     //   }
+     //
+     //   // GET: Factors/Create
+     //   public IActionResult Create()
+     //   {
+     //       return View();
+     //   }
+     //
+     //   // POST: Factors/Create
+     //   // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+     //   // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+     //   [HttpPost]
+     //   [ValidateAntiForgeryToken]
+     //   public async Task<IActionResult> Create([Bind("Id,retailTransport,manuTransport,procTransport,farmerTransport,holdingFactor,roundIncrement,retailProductPrice,manuProductPrice,procProductPrice,farmerProductPrice,harvesterProductPrice,setupCost,initialCapital")] Factors factors)
+     //   {
+     //       if (ModelState.IsValid)
+     //       {
+     //           _context.Add(factors);
+     //           await _context.SaveChangesAsync();
+     //           return RedirectToAction(nameof(Index));
+     //       }
+     //       return View(factors);
+     //   }
 
         // GET: Factors/Edit/5
         public async Task<IActionResult> Edit(string id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var factors = await _context.Factors.FindAsync(id);
-            if (factors == null)
+
+            using (var client = new HttpClient())
             {
-                return NotFound();
+                var response = client.GetAsync(Config.RestApiUrl + "/api/Factors/" + id).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = response.Content;
+                    var responseString = responseContent.ReadAsStringAsync().Result;
+                    if (responseString != null) return View(JsonConvert.DeserializeObject<Factors>(responseString));
+                }
             }
-            return View(factors);
+            return BadRequest();
+
         }
 
         // POST: Factors/Edit/5
@@ -101,66 +105,52 @@ namespace Blockchain_Demonstrator_Web_App.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(string id, [Bind("Id,retailTransport,manuTransport,procTransport,farmerTransport,holdingFactor,roundIncrement,retailProductPrice,manuProductPrice,procProductPrice,farmerProductPrice,harvesterProductPrice,setupCost,initialCapital")] Factors factors)
         {
-            if (id != factors.Id)
+            using (var client = new HttpClient())
             {
-                return NotFound();
-            }
+                var stringContent = new StringContent(JsonConvert.SerializeObject(factors), System.Text.Encoding.UTF8, "application/json");
+                var response = client.PutAsync(Config.RestApiUrl + "/api/Factors/" + id, stringContent).Result;
 
-            if (ModelState.IsValid)
-            {
-                try
+                if (response.IsSuccessStatusCode)
                 {
-                    _context.Update(factors);
-                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index", "Factors");
                 }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!FactorsExists(factors.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
             }
-            return View(factors);
+            return BadRequest();
+           
         }
 
-        // GET: Factors/Delete/5
-        public async Task<IActionResult> Delete(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var factors = await _context.Factors
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (factors == null)
-            {
-                return NotFound();
-            }
-
-            return View(factors);
-        }
-
-        // POST: Factors/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            var factors = await _context.Factors.FindAsync(id);
-            _context.Factors.Remove(factors);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool FactorsExists(string id)
-        {
-            return _context.Factors.Any(e => e.Id == id);
-        }*/
+     //   // GET: Factors/Delete/5
+     //   public async Task<IActionResult> Delete(string id)
+     //   {
+     //       if (id == null)
+     //       {
+     //           return NotFound();
+     //       }
+     //
+     //       var factors = await _context.Factors
+     //           .FirstOrDefaultAsync(m => m.Id == id);
+     //       if (factors == null)
+     //       {
+     //           return NotFound();
+     //       }
+     //
+     //       return View(factors);
+     //   }
+     //
+     //   // POST: Factors/Delete/5
+     //   [HttpPost, ActionName("Delete")]
+     //   [ValidateAntiForgeryToken]
+     //   public async Task<IActionResult> DeleteConfirmed(string id)
+     //   {
+     //       var factors = await _context.Factors.FindAsync(id);
+     //       _context.Factors.Remove(factors);
+     //       await _context.SaveChangesAsync();
+     //       return RedirectToAction(nameof(Index));
+     //   }
+     //
+     //   private bool FactorsExists(string id)
+     //   {
+     //       return _context.Factors.Any(e => e.Id == id);
+     //   }
     }
 }
