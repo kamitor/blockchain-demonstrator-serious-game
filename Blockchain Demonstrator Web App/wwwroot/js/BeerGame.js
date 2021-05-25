341999ad0d1e8f02b4af951e89e6ad4b1445efb9
Unknown-GH <unknown.not.taken@gmail.com>
2021-05-25 16:31:57 +0200

    Added waiting screen to beginning of the game

--- Blockchain Demonstrator Web App/wwwroot/js/BeerGame.js
+++ Blockchain Demonstrator Web App/wwwroot/js/BeerGame.js
@@ -203,79 +203,6 @@ const BeerGame = (() => {
         });
     }
 
-
-    const updateGame = (game) => {
-        gameSerialized = JSON.parse(game);
-        updateOrderHistory("Retailer", gameSerialized.Retailer.CurrentOrder.OrderNumber, gameSerialized.Retailer.CurrentOrder.Volume);
-        updateOrderHistory("Manufacturer", gameSerialized.Manufacturer.CurrentOrder.OrderNumber, gameSerialized.Manufacturer.CurrentOrder.Volume);
-        updateOrderHistory("Processor", gameSerialized.Processor.CurrentOrder.OrderNumber, gameSerialized.Processor.CurrentOrder.Volume);
-        updateOrderHistory("Farmer", gameSerialized.Farmer.CurrentOrder.OrderNumber, gameSerialized.Farmer.CurrentOrder.Volume);
-
-        updateIncomingDeliveries("Retailer", gameSerialized);
-        updateIncomingDeliveries("Manufacturer", gameSerialized);
-        updateIncomingDeliveries("Processor", gameSerialized);
-        updateIncomingDeliveries("Farmer", gameSerialized);
-
-        updateIncomingOrder("Retailer", gameSerialized);
-        updateIncomingOrder("Manufacturer", gameSerialized);
-        updateIncomingOrder("Processor", gameSerialized);
-        updateIncomingOrder("Farmer", gameSerialized);
-
-        $("#balance-Retailer").html("<b>Balance: </b>" + roundOff(gameSerialized.Retailer.Balance));
-        $("#balance-Manufacturer").html("<b>Balance: </b>" + roundOff(gameSerialized.Manufacturer.Balance));
-        $("#balance-Processor").html("<b>Balance: </b>" + roundOff(gameSerialized.Processor.Balance));
-        $("#balance-Farmer").html("<b>Balance: </b>" + roundOff(gameSerialized.Farmer.Balance));
-
-        /*        $("#profit-Retailer").html("<b>Profit: </b>" + roundOff(gameSerialized.retailer.profit));
-                $("#profit-Manufacturer").html("<b>Profit: </b>" + roundOff(gameSerialized.Manufacturer.profit));
-                $("#profit-Processor").html("<b>Profit: </b>" + roundOff(gameSerialized.processor.profit));
-                $("#profit-Farmer").html("<b>Profit: </b>" + roundOff(gameSerialized.farmer.profit));*/
-
-        /*        $("#section-Retailer").html("Margin: " + roundOff(gameSerialized.retailer.margin));
-                $("#section-Manufacturer").html("Margin: " + roundOff(gameSerialized.Manufacturer.margin));
-                $("#section-Processor").html("Margin: " + roundOff(gameSerialized.processor.margin));
-                $("#section-Farmer").html("Margin: " + roundOff(gameSerialized.farmer.margin));*/
-
-        $("#inventory-Retailer").html("<b>Inventory: </b>" + gameSerialized.Retailer.Inventory);
-        $("#inventory-Manufacturer").html("<b>Inventory: </b>" + gameSerialized.Manufacturer.Inventory);
-        $("#inventory-Processor").html("<b>Inventory: </b>" + gameSerialized.Processor.Inventory);
-        $("#inventory-Farmer").html("<b>Inventory: </b>" + gameSerialized.Farmer.Inventory);
-
-        $("#backorder-Retailer").html("<b>Backorder: </b>" + gameSerialized.Retailer.Backorder);
-        $("#backorder-Manufacturer").html("<b>Backorder: </b>" + gameSerialized.Manufacturer.Backorder);
-        $("#backorder-Processor").html("<b>Backorder: </b>" + gameSerialized.Processor.Backorder);
-        $("#backorder-Farmer").html("<b>Backorder: </b>" + gameSerialized.Farmer.Backorder);
-
-        $("#currentDay").text("day: " + gameSerialized.CurrentDay);
-    }
-
-    const updateOrderHistory = (id, orderNumber, orderVolume) => {
-        $(`#orderHistory-${id} > tbody`)
-            .append($(`<tr><td class="order-history">${orderNumber}</td><td class="order-history">${orderVolume}</td></tr>`));
-    }
-
-    const updateIncomingOrder = (id, game) => {
-        game[id].IncomingOrders.forEach(order => {
-            if (order.OrderDay == game.CurrentDay - 7) {
-                $(`#incoming-order-${id}`).html("<b>Incoming order: </b>" + order.Volume);
-            }
-        });
-    }
-
-    const updateIncomingDeliveries = (id, game) => {
-        $(`#incomingDeliveries-${id} > tbody`).empty();
-        game[id].OutgoingOrders.forEach(order => {
-            order.Deliveries.forEach(delivery => {
-                if (delivery.ArrivalDay <= game.CurrentDay && delivery.ArrivalDay > game.CurrentDay - 7) {
-                    $(`#incomingDeliveries-${id} > tbody`)
-                        .append($(`<tr>
-                        <td class="order-history">${delivery.Volume}</td>
-                        </tr>`));
-                }
-            });
-        });
-    }
-
     const joinGame = (gameId, role, name, playerId) => {
         if (configMap.gameId !== null) {
             return $.ajax({
@@ -320,10 +247,7 @@ const BeerGame = (() => {
         init: init,
         getGame: getGame,
         joinGame: joinGame,
-        updateGame: updateGame,
         sendOrders: sendOrders,
-        updateIncomingOrder: updateIncomingOrder,
-        updateIncomingDeliveries: updateIncomingDeliveries,
         promptOptions: promptOptions,
         chooseOption: chooseOption,
         updateGameTuningPage: updateGameTuningPage
@@ -334,17 +258,16 @@ BeerGame.Signal = (() => {
 
     var connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:44393/GameHub").build();
     $("place-order-button").disabled = true;
-
+    
     connection.start().then(function () {
-        console.log("Connection has been made!");
         $("place-order-button").disabled = false;
-        joinGroup();
+        if (BeerGame.Cookie.getCookie("JoinedGame") != null) joinGroup();
     }).catch(function (err) {
         return console.error(err.toString());
     });
 
     let init = () => {
-
+        
     }
 
     let sendOrder = () => {
@@ -358,11 +281,19 @@ BeerGame.Signal = (() => {
     }
 
     let joinGroup = () => {
-        console.log("joined group")
         connection.invoke("JoinGroup", BeerGame.Cookie.getCookie("JoinedGame")).catch(function (err) {
-            return console.error(err.toString())
+            return console.error(err.toString());
         })
     }
+
+    let joinGame = (gameId, role, name, playerId) => {
+        return connection.invoke("JoinGame", gameId, role, name, playerId);
+    }
+
+    connection.on("ShowGame", function () {
+        $(".lds").hide();
+        $(".actor-tab").show();
+    })
 
     connection.on("UpdateGame", function (game) {
         BeerGame.updateGameTuningPage(game);
@@ -378,7 +309,8 @@ BeerGame.Signal = (() => {
 
     return {
         init: init,
-        sendOrder: sendOrder
+        sendOrder: sendOrder,
+        joinGame: joinGame
     }
 })();
 
