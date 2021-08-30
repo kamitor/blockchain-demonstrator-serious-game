49b054f04563f966f45fce10829203de42d42e7a
Shaun Westendorp <56259958+Unknown-GH@users.noreply.github.com>
2021-08-30 15:24:42 +0200

    Update InstallationScript.sh database fix
    Fixed a small oversight for the database not being set up properly when it already exists.

--- InstallationScript.sh
+++ InstallationScript.sh
@@ -18,10 +18,16 @@ sudo apt install nginx -y
 
 #Setup database
 echo "V--------------------Setting up database--------------------V"
-sudo docker pull mcr.microsoft.com/mssql/server:2019-latest
-sudo docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=B33rgam3" \
-   -p 1433:1433 --name database -h database \
-   -d mcr.microsoft.com/mssql/server:2019-latest
+if [[ $(docker ps -a | grep 'Exited.*database') ]]; then
+        sudo docker start database
+elif ! [[ $(docker ps -a | grep 'database') ]]; then
+   sudo docker pull mcr.microsoft.com/mssql/server:2019-latest
+   sudo docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=B33rgam3" \
+      -p 1433:1433 --name database -h database \
+      -d mcr.microsoft.com/mssql/server:2019-latest
+else
+	echo "Database had already been succesfully set up"
+fi
 
 #Setup nginx
 echo "server {
