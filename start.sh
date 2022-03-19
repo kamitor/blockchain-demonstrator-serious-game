53313cfb4787c73a2836125a94c772399c3f381e
Shaun Westendorp <56259958+Unknown-GH@users.noreply.github.com>
2022-03-19 14:14:30 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Run REST API and web app
+echo "V--------------------Running REST API and web app--------------------V"
+url=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+(cd ~/blockchain-demonstrator-serious-game/BlockchainDemonstratorApi/bin/Debug/netcoreapp3.1/publish/
+dotnet BlockchainDemonstratorApi.dll $url) &
+(
+cd ~/blockchain-demonstrator-serious-game/BlockchainDemonstratorWebApp/bin/Debug/netcoreapp3.1/publish
+dotnet BlockchainDemonstratorWebApp.dll $url)
